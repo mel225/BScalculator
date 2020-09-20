451cb485a8da4f9bc6ecf9810554baec3f73a455
y161302 <y161302@edu.tut.ac.jp>
2020-09-20 18:46:14 +0900

    pull

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var NOTE_COEF = {
+  "tap": 1.0,
+  "hold": 0.5,
+  "sidetap": 2.0,
+  "sidehold": 1.0,
+  "flick": 5.0
+  };
+
+var ATTR_COEF = {
+  "firefire": 1,
+  "fireleaf": 1.1,
+  "fireaqua": 0.9,
+  "leaffire": 0.9,
+  "leafleaf": 1,
+  "leafaqua": 1.1,
+  "aquafire": 1.1,
+  "aqualeaf": 0.9,
+  "aquaaqua": 1
+  }
+
 function BScalc(){
   var notes_score = {};
   // 前後半に分けて実行
@@ -6,7 +26,7 @@ function BScalc(){
     // カードの補正攻撃力
     var power = [1,2,3].map(function(n){
       var pow = parseFloat(document.querySelector(`input[name=pow_${half}${n}]`).value);
-      var attr_coef = getAttrCoef(getAttr(n), getAttr());
+      var attr_coef = ATTR_COEF[getAttr(n) + getAttr()];
       var diff_coef = parseFloat(document.querySelector("select[name=difficulty]").value);
       var level_coef = 199 + parseInt(document.querySelector("input[name=boss_level]").value);
       return new BigNumber(pow).times(attr_coef).times(diff_coef).times(level_coef).toNumber();
@@ -19,7 +39,7 @@ function BScalc(){
       var notes_num = document.querySelector(`input[name=${note}_${half}]`).value;
       var bs_notes_num = document.querySelector("input[name=bsnum_notes]").value;
       power.forEach(function(npow){
-        score += Math.ceil(new BigNumber(npow).times(getNoteCoef(note)).div(bs_notes_num).times(100).div(3).toNumber());
+        score += Math.ceil(new BigNumber(npow).times(NOTE_COEF[note]).div(bs_notes_num).times(100).div(3).toNumber());
       });
       document.querySelector(`label[name=unibs_${note}_${half}]`).innerText = score;
       document.querySelector(`label[name=bs_${note}_${half}]`).innerText = score * notes_num;
@@ -43,50 +63,23 @@ function BScalc(){
 }
 
 function getAttr(n){
-  if(!n) n="";
-  var attr;
-  ["fire", "leaf", "aqua"].forEach(function(a){
-    if(document.querySelector(`input[id=attr_${a}${n}]`).checked){
-      attr = a;
-    }
-  });
-  return attr;
+  if(!n)
+    return document.querySelector("input[name=attr_boss]").value;
+  else
+    return document.querySelector(`input[name=attr_card${n}]`).value;
 }
 
-function getAttrCoef(user_attr, enemy_attr){
-  var coef;
-  switch(user_attr + enemy_attr){
-    case "firefire": coef = 1; break;
-    case "fireleaf": coef = 1.1; break;
-    case "fireaqua": coef = 0.9; break;
-    case "leaffire": coef = 0.9; break;
-    case "leafleaf": coef = 1; break;
-    case "leafaqua": coef = 1.1; break;
-    case "aquafire": coef = 1.1; break;
-    case "aqualeaf": coef = 0.9; break;
-    case "aquaaqua": coef = 1; break;
-  }
-  return coef;
-}
-
-function bs_change(input){
+function bs_change(){
   var num = 0;
   ["first", "boss"].forEach(function(half){
     ["tap", "hold", "sidetap", "sidehold", "flick"].forEach(function(note){
-      num = new BigNumber(document.querySelector(`input[name=${note}_${half}]`).value).times(getNoteCoef(note)).plus(num);
+      num = new BigNumber(document.querySelector(`input[name=${note}_${half}]`).value).times(NOTE_COEF[note]).plus(num);
     });
   });
   document.querySelector("input[name=bsnum_notes]").value = num.toNumber();
 }
 
-function getNoteCoef(note_name){
-  var result;
-  switch(note_name){
-    case "tap": result = 1; break;
-    case "hold": result = 0.5; break;
-    case "sidetap": result = 2; break;
-    case "sidehold": result = 1; break;
-    case "flick": result = 5; break;
-  }
-  return result;
+function attr_change(radio){
+  console.log(radio);
+  document.querySelector(`input[name=attr_${radio.name.split("_")[0]}]`).value = radio.value;
 }
